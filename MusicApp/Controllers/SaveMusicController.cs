bda7a95d0de02a37e380e198f79b3445ae2a5c9f
Axel <axelolmar@gmail.com>
2023-10-12 17:46:51 -0600

    Creación controlador para guardar musica(axel)

---
+++ MusicApp/Controllers/SaveMusicController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TagLib;
+
+namespace MusicApp.Controllers
+{
+    public class SaveMusicController : Controller
+    {
+        // GET: SaveMusic
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private ActionResult ObtenerMetadatos(string filePath)
+        {
+            
+
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult GuardarMetadatos(string filePath)
+        {
+
+            return null;
+        }
+    }
+}
